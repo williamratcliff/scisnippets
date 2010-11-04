ed377ae8012c22fc3e54f5a3d1134dda4f636c65
William Ratcliff <william.ratcliff@gmail.com>
2010-11-03 22:39:01 -0400

    some urls now working

--- scisnippets/urls.py
+++ scisnippets/urls.py
@@ -4,15 +4,22 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
+
+
 urlpatterns = patterns('',
     # Example:
+    (r'^tags',include('external_apps.cab.urls.tags')),
+    (r'^bookmarks',include('external_apps.cab.urls.bookmarks')),
+    (r'^languages',include('external_apps.cab.urls.languages')),
+    (r'^popular',include('external_apps.cab.urls.popular')),
+    (r'^snippets',include('external_apps.cab.urls.snippets')),
     # (r'^scisnippets/', include('scisnippets.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     (r'^admin/', include(admin.site.urls)),
+     #(r'^admin/', include(admin.site.urls)),
 )
 
-#urlpatterns += patterns()
+#urlpatterns += include('external_apps.cab.urls')
