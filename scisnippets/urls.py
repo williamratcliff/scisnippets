a2c42a63b4f15e47b1a1bff6433568767cb900c7
William Ratcliff <william.ratcliff@gmail.com>
2010-11-04 22:14:36 -0400

    have created a snippet, now can we see it?

--- scisnippets/urls.py
+++ scisnippets/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     #(r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
 
 #urlpatterns += include('external_apps.cab.urls')
