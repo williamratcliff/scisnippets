255f9b80080f608cb015b78b97b95b4806519b31
William Ratcliff <william.ratcliff@gmail.com>
2010-11-05 02:56:40 -0400

    more urls work, we have css.  Search needs to be fixed.  Downloaads need to guess the file name better.  Comments are broken

--- scisnippets/settings.py
+++ scisnippets/settings.py
@@ -105,7 +105,11 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'templates')
+    os.path.join(SITE_ROOT, 'templates'),
+    os.path.join(EXTERNAL_APPS, 'cab/templates'),
+    os.path.join(EXTERNAL_APPS, 'cab/templates/cab'),
+    os.path.join(EXTERNAL_APPS, 'cab/templates/cab/templatetags'),
+    os.path.join(EXTERNAL_APPS, 'cab/templates/ratings/templatetags'),
 )
 
 
